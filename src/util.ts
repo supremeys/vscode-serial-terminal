8bda81bb60c2c487b7ff6bfcf714befd1ddc0733
riphiphip <halvor.bjorstad@gmail.com>
2020-07-24 08:41:52 +0200

    improve handling of escaped text

--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,19 @@
 
-const escapeRe = /\\(.)/g;
-const hexCharRe = /\\x([a-fA-F0-9]{2})/;
-const uCharRe = /\\u([a-fA-F0-9]{4})/;
+const escapeRe = /\\(.)/;
+const hexCharRe = /(?<!\\)\\x([a-fA-F0-9]{2})/;
+const uCharRe = /(?<!\\)\\u([a-fA-F0-9]{4})/;
 
 export function unescape(original: string): string {
 
+    let matches;
+    while ((matches = hexCharRe.exec(original)) !== null) {
+        original = original.replace(matches[0], String.fromCharCode(parseInt(matches[1], 16)));
+    }
+    while ((matches = uCharRe.exec(original)) !== null) {
+        original = original.replace(matches[0], String.fromCharCode(parseInt(matches[1], 16)));
+    }
     if (escapeRe.test(original)) {
         let lookup: { [key: string]: string } = {
-            "\\\"": "\"",
-            "\\\'": "\'",
             "\\b": "\b",
             "\\f": "\f",
             "\\n": "\n",
@@ -16,18 +21,11 @@ export function unescape(original: string): string {
             "\\t": "\t",
             "\\v": "\v",
             "\\0": "\0",
-            "\\\\": "\\",
         };
         for (let exp in lookup) {
             original = original.replace(exp, lookup[exp]);
         }
-    }
-    let matches;
-    while ((matches = hexCharRe.exec(original)) !== null) {
-        original = original.replace(matches[0], String.fromCharCode(parseInt(matches[1], 16)));
-    }
-    while ((matches = uCharRe.exec(original)) !== null) {
-        original = original.replace(matches[0], String.fromCharCode(parseInt(matches[1], 16)));
+        original = original.replace(/\\(.?)/g, "$1");
     }
     return original;
 }
